6a4fd7fbe1875ff01e7771c5dbcd0d10d777fe94
yarduon <yard.campbs@gmail.com>
2023-04-02 16:34:00 +0100

    Add simple paint functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+        console.log("helou");
+        square.style.backgroundColor = "black";
+    })
     document.getElementById("canvas").appendChild(square);
   }
 }
