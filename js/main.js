23dbf51877d6106a285b77f4fee0e8bad6ef0b95
yarduon <yard.campbs@gmail.com>
2023-04-02 15:13:02 +0100

    Add basic pixel canvas

---
+++ js/main.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    document.getElementById("canvas").appendChild(square);
+  }
+}
