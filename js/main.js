168bc625461303c2ddcc7d0860110694106e3244
yarduon <yard.campbs@gmail.com>
2023-04-02 16:50:38 +0100

    Change size pixel canvas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-for (let i = 0; i < 16; i++) {
-  for (let i = 0; i < 16; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", () => {
-        console.log("helou");
+function createCanvas(max) {
+  for (let i = 0; i < max; i++) {
+    for (let i = 0; i < max; i++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = (960 / max) + "px";
+      square.style.height = (960 /max) + "px";
+      square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "black";
-    })
-    document.getElementById("canvas").appendChild(square);
+      });
+      document.getElementById("canvas").appendChild(square);
+    }
   }
 }
+
+createCanvas(16);
+
+document.getElementById("size").addEventListener("click", () => {
+    document.getElementById("canvas").innerHTML = "";
+    createCanvas(prompt("Introduce size"));
+})
